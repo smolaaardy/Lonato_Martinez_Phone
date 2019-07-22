1c2ddbe49fa69c1c18ed8acc10dba13264c23812
smolaaardy <50385566+smolaaardy@users.noreply.github.com>
2019-07-22 11:41:45 -0500

    Add files via upload

---
+++ src/Ringable.java
@@ -0,0 +1,15 @@
+
+package com.codingdojo.java.phone;
+
+public interface Ringable {
+
+	public default String ring() {
+		return "Bzz Bzz Bzz!";
+	}
+	public default String unlock() {
+		return "Click! - Phone unlocked!";
+	}
+	
+	
+	
+}
